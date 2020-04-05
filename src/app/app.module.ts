92d9916ce61329cadcf6ad2c759a333884150ea2
1921622004 <1921622004qwer@gmail.com>
2020-04-05 17:15:33 +0800

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { routes } from './routes';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TagDetailComponent } from './pages/tag-detail/tag-detail.component';
+import { QuestionDetailComponent } from './pages/question-detail/question-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    TagDetailComponent,
+    QuestionDetailComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
